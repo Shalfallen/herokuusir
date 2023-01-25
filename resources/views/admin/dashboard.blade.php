49d081bfaaea23d5e128fe09ed8b7fdfe4adce68
Shalfallen <shaltear33@gmail.com>
2023-01-25 16:08:03 +0700

    commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="active">Dashboard</li>
+@endsection
+
+@section('content')
+<!-- Small boxes (Stat box) -->
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>{{ $kategori }}</h3>
+
+                <p>Total Kategori</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-cube"></i>
+            </div>
+            <a href="{{ route('kategori.index') }}" class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>{{ $produk }}</h3>
+
+                <p>Total Produk</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-cubes"></i>
+            </div>
+            <a href="{{ route('produk.index') }}" class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-6 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-red">
+            <div class="inner">
+                <h3>{{ $supplier }}</h3>
+
+                <p>Total Supplier</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-truck"></i>
+            </div>
+            <a href="{{ route('supplier.index') }}" class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+</div>
+<!-- /.row -->
+
+{{-- row tambahan --}}
+<div class="row">
+    <div class="col-lg-12">
+        <!-- small box -->
+        <div class="small-box bg-purple">
+            <div class="inner">
+                @php
+                    $val = 0;
+                @endphp
+                @foreach ($data_pendapatan as $data)
+                <div style="display:none">{{ $val += $data }}</div>
+                @endforeach
+                <h3>{{'Rp. ' . number_format($val)}}</h3>
+                <p>Pendapatan bulan ini</p>
+                <p>{{date('M')}}</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-id-card"></i>
+            </div>
+            <a href="{{ route('laporan.index') }}" class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <div class="col-lg-12">
+        <!-- small box -->
+        <div class="small-box bg-yellow">
+            <div class="inner">
+                <h3>{{'Rp. ' . number_format($data_pendapatan[date('d') -1]) }}</h3>
+                <p>Pendapatan hari ini</p>
+                <p>{{$tanggal_akhir}}</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-id-card"></i>
+            </div>
+            <a href="{{ route('laporan.index') }}" class="small-box-footer">Lihat <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+
+<!-- Main row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Grafik Pendapatan [bulanan] {{ tanggal_indonesia($tanggal_awal, false) }} s/d {{ tanggal_indonesia($tanggal_akhir, false) }}</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="chart">
+                            <!-- Sales Chart Canvas -->
+                            <canvas id="salesChart" style="height: 180px;"></canvas>
+                        </div>
+                        <!-- /.chart-responsive -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
+<!-- /.row (main row) -->
+@endsection
+
+@push('scripts')
+<!-- ChartJS -->
+<script src="{{ asset('AdminLTE-2/bower_components/chart.js/Chart.js') }}"></script>
+<script>
+$(function() {
+    // Get context with jQuery - using jQuery's .get() method.
+    var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
+    // This will get the first returned node in the jQuery collection.
+    var salesChart = new Chart(salesChartCanvas);
+
+    var salesChartData = {
+        labels: {{ json_encode($data_tanggal) }},
+        datasets: [
+            {
+                label: 'Pendapatan',
+                fillColor           : 'rgba(60,141,188,0.9)',
+                strokeColor         : 'rgba(60,141,188,0.8)',
+                pointColor          : '#3b8bba',
+                pointStrokeColor    : 'rgba(60,141,188,1)',
+                pointHighlightFill  : '#fff',
+                pointHighlightStroke: 'rgba(60,141,188,1)',
+                data: {{ json_encode($data_pendapatan) }}
+            }
+        ]
+    };
+
+    var salesChartOptions = {
+        pointDot : false,
+        responsive : true
+    };
+
+    salesChart.Line(salesChartData, salesChartOptions);
+});
+</script>
+@endpush
